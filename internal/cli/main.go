49c98a582386d7ae9eebee02054b8e534e8dfaf2
suraciii <suraciii@outlook.com>
2026-08-13 16:59:01 +0800

    organize the CLI into internal/cli behind a root main stub
    The root main.go is a thin stub calling cli.Run, so go install
    github.com/suraciii/pages@latest works while the command implementation
    stays organized in internal/cli, like hugo and other single-command
    tools.

---
+++ internal/cli/main.go
@@ -0,0 +1,28 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+)
+
+func Run(args []string) int {
+	if len(args) == 0 {
+		printUsage(os.Stderr)
+		return 2
+	}
+	switch args[0] {
+	case "--help", "-h":
+		printUsage(os.Stdout)
+		return 0
+	case "serve":
+		return runServe(args[1:])
+	case "publish":
+		return runPublish(args[1:])
+	case "generate-token":
+		return runGenerateToken(args[1:])
+	default:
+		fmt.Fprintf(os.Stderr, "pages: unknown command %q\n", args[0])
+		printUsage(os.Stderr)
+		return 2
+	}
+}
--- main.go
+++ main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/suraciii/pages/internal/cli"
 )
 
 func main() {
-	os.Exit(run(os.Args[1:]))
-}
-
-func run(args []string) int {
-	if len(args) == 0 {
-		printUsage(os.Stderr)
-		return 2
-	}
-	switch args[0] {
-	case "--help", "-h":
-		printUsage(os.Stdout)
-		return 0
-	case "serve":
-		return runServe(args[1:])
-	case "publish":
-		return runPublish(args[1:])
-	case "generate-token":
-		return runGenerateToken(args[1:])
-	default:
-		fmt.Fprintf(os.Stderr, "pages: unknown command %q\n", args[0])
-		printUsage(os.Stderr)
-		return 2
-	}
+	os.Exit(cli.Run(os.Args[1:]))
 }
